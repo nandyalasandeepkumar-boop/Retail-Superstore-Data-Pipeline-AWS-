5a12601f5e2be8cd04baac3557bcd676a440e232
nandyalasandeepkumar-boop <nandyalasandeepkumar3786@gmail.com>
2025-08-12 21:04:41 -0500

    Create variables.tf

---
+++ Terraform — infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "Project prefix"
+  type        = string
+  default     = "retail-superstore"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
